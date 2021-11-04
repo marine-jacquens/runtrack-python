8494a440d7da7ad356ee182876622e25fd4d0d2c
“marine-jacquens” <62135610+marine-jacquens@users.noreply.github.com>
2021-11-04 12:26:27 +0100

    job03
    calculer le nbr de mots qui comptent le nbr de lettre(s) identique(s) au nbr entré dans l'input

---
+++ jour03/job03/main.py
@@ -0,0 +1,21 @@
+import re
+
+def program():
+    nbr = int(input('Entrez un nombre entier '))
+    doc = open('../job02/data.txt','r')
+    a = doc.readline()
+    x = re.findall(r"([a-zA-z]{1,})", a)
+
+    compteur = 0
+
+    n = 0
+    while n < len(x):
+
+        if(len(x[n]) == nbr):
+            compteur+=1
+        n+=1
+    
+    print(compteur)
+
+
+program()
