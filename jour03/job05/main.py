8c8a988ebd9c7d5223bf4f421ab4f02209c25110
“marine-jacquens” <62135610+marine-jacquens@users.noreply.github.com>
2021-11-04 18:59:59 +0100

    Update main.py

--- jour03/job05/main.py
+++ jour03/job05/main.py
@@ -7,28 +7,28 @@ import matplotlib as mlp
 def program():
 
     doc = open('../job02/data.txt','r')
-    
+
     alphabetMaj = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     alphabetMin = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     alphabetDict = {'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'I':0,'J':0,'K':0,'L':0,'M':0,'N':0,'O':0,'P':0,'Q':0,'R':0,'S':0,'T':0,'U':0,'V':0,'W':0,'X':0,'Y':0,'Z':0}
     
-    for a in doc:
-        x = re.findall(r"[a-z]",a,re.IGNORECASE)
-        n = 0 
+    a = doc.read()
+    x = re.findall(r"[a-z]",a,re.IGNORECASE)
+    n = 0 
 
-        while n < len(x):
+    while n < len(x):
 
-            b = 0
+        b = 0
 
-            while b < 26:
+        while b < 26:
 
-                if(x[n] == alphabetMaj[b] or x[n] == alphabetMin[b]):
-                    letter = alphabetMaj[b]
-                    alphabetDict[letter] = alphabetDict.get(letter)+1
+            if(x[n] == alphabetMaj[b] or x[n] == alphabetMin[b]):
+                letter = alphabetMaj[b]
+                alphabetDict[letter] = alphabetDict.get(letter)+1
 
-                b+=1
+            b+=1
                 
-            n+=1
+        n+=1
     
     print('dictionnaire : ', alphabetDict)
     
